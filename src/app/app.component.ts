c8484659eda73dcdc75109ebbe1091d4a046868b
brandroid <brandroid@gmail.com>
2021-02-03 11:38:19 -0500

    get some things happening

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { sharedStylesheetJitUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'food list';
-  inventory = ['beef', 'potatoes', 'carrots', 'peas', 'onions', 'flour', 'tomato sauce', 'seasoning']
+  inventory = ['beef', 'potatoes', 'carrots', 'peas', 'onions', 'flour', 'tomato sauce', 'seasoning'];
+  newFood = '';
+  addToFoodList() {
+    this.inventory.push (this.newFood)
+  }
+  removeFromFoodList(index) {
+    this.inventory.splice (index, 1)
+  }
 }
