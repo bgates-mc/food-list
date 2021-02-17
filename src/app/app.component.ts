645065fa1f12be14a567abd100e36e10ba09f2ea
brandroid <brandroid@gmail.com>
2021-02-17 10:46:31 -0500

    dupe check and forced lowercase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,50 @@
-import { sharedStylesheetJitUrl } from '@angular/compiler';
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'food list';
-  inventory = ['beef', 'potatoes', 'carrots', 'peas', 'onions', 'flour', 'tomato sauce', 'seasoning'];
-  newFood = '';
+  title = "food list";
+  inventory = ["beef", "potatoes", "carrots", "peas", "onions", "flour", "tomato sauce", "seasoning"];
+  newFood = "";
   addToFoodList() {
-    this.inventory.push (this.newFood)
+    if (this.newFoodValidator()) {
+      this.inventory.push(this.newFood.trim().toLowerCase());
+      this.newFood = "";
+    }
+  }
+  newFoodValidator() {
+    if (this.newFood.trim() !== "") {
+      return !this.inventory.includes(this.newFood.trim().toLowerCase());
+    }
+    return false;
+  }
+  newFoodValidator2() {
+    if (this.newFood.trim() === "") {
+      return false;
+    }
+
+    if (this.inventory.includes(this.newFood.trim().toLowerCase())) {
+      return false;
+    }
+
+    return true;
+  }
+  newFoodValidator3() {
+    return this.newFood.trim() !== "" && !this.inventory.includes(this.newFood.trim().toLowerCase());
+  }
+  newFoodValidator4() {
+    const isBlank = this.newFood.trim() === "";
+    const isDuplicate = this.inventory.includes(this.newFood.trim().toLowerCase());
+
+    return !(isBlank || isDuplicate);
   }
   removeFromFoodList(index) {
-    this.inventory.splice (index, 1)
+    this.inventory.splice(index, 1);
   }
 }
+
+// what's next?
+// because I'm eager to start getting into some more designy things but need to continue to lay a more traditional foundation of knowledge first, maybe we could give these things a random color when added or something?
